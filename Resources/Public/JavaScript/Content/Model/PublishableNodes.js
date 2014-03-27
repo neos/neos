mneuhaus/TYPO3.Neos

[TASK] Ext.Direct replacement
This change removes the dependency to the TYPO3.ExtJS package. It replaces
the usage of the global ExtDirect controllers and uses instances of
the HttpClient which should from now on be used for server communication.

The HttpClient handling uses Ember.Deferred. Also the usages of
$.Deferred are replaced by Ember.Deferred.

This change will be the basis for more refactoring in the server side
services and JavaScript code, but is already a good improvement.

Depends on: I54651fda4e84a8fce4fef3343ac42cd87abacaae

Releases: master, 1.1
Change-Id: I471ffd18fd8da706b0dccc4fcdd7aacbfd5dd909

Resources/Public/JavaScript/Content/Model/PublishableNodes.js
@@ -11,7 +11,9 @@ define(
 	'vie/entity',
 	'Shared/EventDispatcher',
 	'Shared/NodeTypeService',
-	'Shared/Notification'
+	'Shared/Notification',
+	'Shared/Endpoint/WorkspaceEndpoint',
+	'Shared/Endpoint/NodeEndpoint'
 ], function(
 	Ember,
 	$,
@@ -19,7 +21,9 @@ define(
 	EntityWrapper,
 	EventDispatcher,
 	NodeTypeService,
-	Notification
+	Notification,
+	WorkspaceEndpoint,
+	NodeEndpoint
 ) {
 	return Ember.Object.extend({
 		publishableEntitySubjects: [],
@@ -91,76 +95,95 @@ define(
 		 * Publish all blocks which are unsaved *and* on current page.
 		 */
 		publishChanges: function(autoPublish) {
-			var that = this;
-			T3.Content.Controller.ServerConnection.sendAllToServer(
-				this.get('publishableEntitySubjects'),
-				function(subject) {
+			var that = this,
+				transformFn = function(subject) {
 					var entity = vie.entities.get(subject);
 					return [entity.fromReference(subject), 'live'];
 				},
-				TYPO3_Neos_Service_ExtDirect_V1_Controller_WorkspaceController.publishNode,
-				function() {
-					if (autoPublish != true) {
-						var nodeTypeSchema = NodeTypeService.getCurrentNodeTypeSchema(),
-							title = $('#neos-page-metainformation').attr('data-neos-title')
-						Notification.ok('Published changes for ' + nodeTypeSchema.ui.label + ' "' + title + '".');
-					}
-				},
-				function(subject) {
-					var entity = vie.entities.get(subject);
-					entity.set('typo3:__workspacename', 'live');
+				numberOfUnsavedRecords = this.get('publishableEntitySubjects').get('length');
 
-					var nodePath = entity.id.substr(1, entity.id.lastIndexOf('@') - 1),
-						node = that.get('workspaceWidePublishableEntitySubjects').findBy('nodePath', nodePath);
-					if (node) {
-						that.get('workspaceWidePublishableEntitySubjects').removeObject(node);
+			this.get('publishableEntitySubjects').forEach(function(element) {
+				// Force copy of array
+				var args = transformFn(element).slice();
+				NodeEndpoint.set('_saveRunning', true);
+
+				NodeEndpoint.update(args[0]).then(
+					function() {
+						that._removeNodeFromPublishableEntitySubjects(element, 'live');
+						numberOfUnsavedRecords--;
+						if (numberOfUnsavedRecords <= 0) {
+							NodeEndpoint.set('_saveRunning', false);
+
+							if (autoPublish != true) {
+								var nodeTypeSchema = NodeTypeService.getCurrentNodeTypeSchema(),
+									title = $('#neos-page-metainformation').attr('data-neos-title')
+								Notification.ok('Published changes for ' + nodeTypeSchema.ui.label + ' "' + title + '".');
+							}
+						}
 					}
-				}
-			);
+				);
+			});
+		},
+
+		_removeNodeFromPublishableEntitySubjects: function(subject, workspaceOverride) {
+			var that = this,
+				entity = vie.entities.get(subject);
+			if (workspaceOverride) {
+				entity.set('typo3:__workspacename', workspaceOverride);
+			}
+
+			var nodePath = entity.id.substr(1, entity.id.lastIndexOf('@') - 1),
+				node = that.get('workspaceWidePublishableEntitySubjects').findBy('nodePath', nodePath);
+			if (node) {
+				that.get('workspaceWidePublishableEntitySubjects').removeObject(node);
+			}
 		},
 
 		/**
 		 * Discard all blocks which are unsaved *and* on current page.
 		 */
 		discardChanges: function() {
-			var that = this;
-			T3.Content.Controller.ServerConnection.sendAllToServer(
-				this.get('publishableEntitySubjects'),
-				function(subject) {
+			var that = this,
+				transformFn = function(subject) {
 					var entity = vie.entities.get(subject);
 					return [entity.fromReference(subject)];
 				},
-				TYPO3_Neos_Service_ExtDirect_V1_Controller_WorkspaceController.discardNode,
-				function() {
-					require(
-						{context: 'neos'},
-						[
-							'Content/Application'
-						],
-						function(ContentModule) {
-							ContentModule.reloadPage();
-							ContentModule.one('pageLoaded', function() {
-								Ember.run.next(function() {
-									EventDispatcher.trigger('nodesInvalidated');
-									EventDispatcher.trigger('contentChanged');
-								});
-							});
-						}
-					);
-					var nodeTypeSchema = NodeTypeService.getCurrentNodeTypeSchema(),
-						title = $('#neos-page-metainformation').attr('data-neos-title')
-					Notification.ok('Discarded changes for ' + nodeTypeSchema.ui.label + ' "' + title + '".');
-				},
-				function(subject) {
-					var entity = vie.entities.get(subject);
+				numberOfUnsavedRecords = this.get('publishableEntitySubjects').get('length');
 
-					var nodePath = entity.id.substr(1, entity.id.lastIndexOf('@') - 1),
-						node = that.get('workspaceWidePublishableEntitySubjects').findBy('nodePath', nodePath);
-					if (node) {
-						that.get('workspaceWidePublishableEntitySubjects').removeObject(node);
+			this.get('publishableEntitySubjects').forEach(function(element) {
+				// Force copy of array
+				var args = transformFn(element).slice();
+				NodeEndpoint.set('_saveRunning', true);
+
+				NodeEndpoint.discardNode(args[0]).then(
+					function() {
+						that._removeNodeFromPublishableEntitySubjects(element);
+						numberOfUnsavedRecords--;
+						if (numberOfUnsavedRecords <= 0) {
+							NodeEndpoint.set('_saveRunning', false);
+
+							require(
+								{context: 'neos'},
+								[
+									'Content/Application'
+								],
+								function(ContentModule) {
+									ContentModule.reloadPage();
+									ContentModule.one('pageLoaded', function() {
+										Ember.run.next(function() {
+											EventDispatcher.trigger('nodesInvalidated');
+											EventDispatcher.trigger('contentChanged');
+										});
+									});
+								}
+							);
+							var nodeTypeSchema = NodeTypeService.getCurrentNodeTypeSchema(),
+								title = $('#neos-page-metainformation').attr('data-neos-title');
+							Notification.ok('Discarded changes for ' + nodeTypeSchema.ui.label + ' "' + title + '".');
+						}
 					}
-				}
-			);
+				);
+			});
 		},
 
 		/**
@@ -171,19 +194,20 @@ define(
 				workspaceName = siteRoot.substr(siteRoot.lastIndexOf('@') + 1),
 				publishableEntities = this.get('publishableEntitySubjects'),
 				that = this;
-			TYPO3_Neos_Service_ExtDirect_V1_Controller_WorkspaceController.publishAll(workspaceName, function(result) {
-				if (typeof result !== 'undefined' && result !== null && result.success === true) {
+
+			WorkspaceEndpoint.publishAll(workspaceName).then(
+				function() {
 					$.each(publishableEntities, function(index, element) {
 						vie.entities.get(element).set('typo3:__workspacename', 'live');
 					});
 
 					that.getWorkspaceWideUnpublishedNodes();
-
 					Notification.ok('Published all changes.');
-				} else {
+				},
+				function() {
 					Notification.error('Unexpected error while publishing all changes: ' + JSON.stringify(result));
 				}
-			});
+			);
 		},
 
 		/**
@@ -193,8 +217,8 @@ define(
 			var siteRoot = $('#neos-page-metainformation').attr('data-__siteroot'),
 				workspaceName = siteRoot.substr(siteRoot.lastIndexOf('@') + 1),
 				that = this;
-			TYPO3_Neos_Service_ExtDirect_V1_Controller_WorkspaceController.discardAll(workspaceName, function(result) {
-				if (typeof result !== 'undefined' && result !== null && result.success === true) {
+			WorkspaceEndpoint.discardAll(workspaceName).then(
+				function() {
 					require(
 						{context: 'neos'},
 						[
@@ -212,10 +236,11 @@ define(
 					);
 					that.getWorkspaceWideUnpublishedNodes();
 					Notification.ok('Discarded all changes.');
-				} else {
+				},
+				function() {
 					Notification.error('Unexpected error while discarding all changes: ' + JSON.stringify(result));
 				}
-			});
+			);
 		},
 
 		/**
@@ -225,11 +250,12 @@ define(
 			var siteRoot = $('#neos-page-metainformation').attr('data-__siteroot'),
 				workspaceName = siteRoot.substr(siteRoot.lastIndexOf('@') + 1),
 				that = this;
-			if (typeof TYPO3_Neos_Service_ExtDirect_V1_Controller_WorkspaceController !== 'undefined') {
-				TYPO3_Neos_Service_ExtDirect_V1_Controller_WorkspaceController.getWorkspaceWideUnpublishedNodes(workspaceName, function(result) {
+
+			WorkspaceEndpoint.getWorkspaceWideUnpublishedNodes(workspaceName).then(
+				function(result) {
 					that.set('workspaceWidePublishableEntitySubjects', result.data);
-				});
-			}
+				}
+			);
 		}
 
 	}).create();

neos/neos

Reviewed-on: https://review.typo3.org/25204
Reviewed-by: Rens Admiraal
Tested-by: Rens Admiraal
Reviewed-by: Aske Ertmann
Reviewed-by: Markus Goldbeck
Tested-by: Markus Goldbeck
Original-Commit-Hash: 3be41482bca473316a4aef4b4c54a4c63ba0c9bf

Resources/Public/JavaScript/Content/Model/PublishableNodes.js
@@ -1,5 +1,5 @@
 /**
- * PublishableNodes
+ * Publishable nodes
  *
  * Contains the currently publishable (proxy) nodes.
  */
@@ -10,20 +10,23 @@ define(
 	'vie/instance',
 	'vie/entity',
 	'Shared/EventDispatcher',
-	'Shared/NodeTypeService',
-	'Shared/Notification'
+	'Shared/ResourceCache',
+	'Shared/Configuration',
+	'Shared/Notification',
+	'Shared/Endpoint/WorkspaceEndpoint'
 ], function(
 	Ember,
 	$,
 	vie,
 	EntityWrapper,
 	EventDispatcher,
-	NodeTypeService,
-	Notification
+	ResourceCache,
+	Configuration,
+	Notification,
+	WorkspaceEndpoint
 ) {
 	return Ember.Object.extend({
 		publishableEntitySubjects: [],
-
 		workspaceWidePublishableEntitySubjects: [],
 
 		noChanges: function() {
@@ -56,6 +59,9 @@ define(
 			});
 		},
 
+		/**
+		 * @return {void}
+		 */
 		_updatePublishableEntities: function() {
 			var publishableEntitySubjects = [];
 			var pageNodeContextPath = $('#neos-page-metainformation').attr('about'),
@@ -81,6 +87,9 @@ define(
 		/**
 		 * Check whether the entity is publishable or not. Currently, everything
 		 * which is not in the live workspace is publishable.
+		 *
+		 * @param {object} entity
+		 * @return {boolean}
 		 */
 		_isEntityPublishable: function(entity) {
 			var attributes = EntityWrapper.extractAttributesFromVieEntity(entity);
@@ -89,110 +98,151 @@ define(
 
 		/**
 		 * Publish all blocks which are unsaved *and* on current page.
+		 *
+		 * @param {mixed} autoPublish
+		 * @return {void}
 		 */
 		publishChanges: function(autoPublish) {
-			var that = this;
-			T3.Content.Controller.ServerConnection.sendAllToServer(
-				this.get('publishableEntitySubjects'),
-				function(subject) {
+			var that = this,
+				targetWorkspace = 'live',
+				transformFn = function(subject) {
 					var entity = vie.entities.get(subject);
-					return [entity.fromReference(subject), 'live'];
+					return [entity.fromReference(subject), targetWorkspace];
 				},
-				TYPO3_Neos_Service_ExtDirect_V1_Controller_WorkspaceController.publishNode,
-				function() {
-					if (autoPublish != true) {
-						var nodeTypeSchema = NodeTypeService.getCurrentNodeTypeSchema(),
-							title = $('#neos-page-metainformation').attr('data-neos-title')
-						Notification.ok('Published changes for ' + nodeTypeSchema.ui.label + ' "' + title + '".');
-					}
-				},
-				function(subject) {
-					var entity = vie.entities.get(subject);
-					entity.set('typo3:__workspacename', 'live');
+				numberOfUnsavedRecords = this.get('publishableEntitySubjects.length');
 
-					var nodePath = entity.id.substr(1, entity.id.lastIndexOf('@') - 1),
-						node = that.get('workspaceWidePublishableEntitySubjects').findBy('nodePath', nodePath);
-					if (node) {
-						that.get('workspaceWidePublishableEntitySubjects').removeObject(node);
+			this.get('publishableEntitySubjects').forEach(function(element) {
+				// Force copy of array
+				var args = transformFn(element).slice();
+
+				WorkspaceEndpoint.publishNode(args[0], targetWorkspace).then(
+					function() {
+						that._removeNodeFromPublishableEntitySubjects(element, 'live');
+						numberOfUnsavedRecords--;
+						if (numberOfUnsavedRecords <= 0) {
+							if (autoPublish !== true) {
+								var nodeType, title = $('#neos-page-metainformation').attr('data-neos-title');
+								// TODO: The fetching of the current node type should be handled using the NodeTypeService
+								// which at the moment causes issues with promises which still have to be optimized
+								ResourceCache.getItem(Configuration.get('NodeTypeSchemaUri') + '&superType=TYPO3.Neos:Document').then(
+									function(schema) {
+										nodeType = schema[$('#neos-page-metainformation').attr('data-neos-__nodetype')].ui.label;
+										Notification.ok('Published changes for ' + nodeType + ' "' + title + '"');
+									}
+								);
+							}
+						}
 					}
-				}
-			);
+				);
+			});
+		},
+
+		/**
+		 * @param {string} subject
+		 * @param {string} workspaceOverride
+		 * @return {void}
+		 */
+		_removeNodeFromPublishableEntitySubjects: function(subject, workspaceOverride) {
+			var that = this,
+				entity = vie.entities.get(subject);
+			if (workspaceOverride) {
+				entity.set('typo3:__workspacename', workspaceOverride);
+			}
+
+			var nodePath = entity.id.substr(1, entity.id.lastIndexOf('@') - 1),
+				node = that.get('workspaceWidePublishableEntitySubjects').findBy('nodePath', nodePath);
+			if (node) {
+				that.get('workspaceWidePublishableEntitySubjects').removeObject(node);
+			}
 		},
 
 		/**
 		 * Discard all blocks which are unsaved *and* on current page.
+		 *
+		 * @return {void}
 		 */
 		discardChanges: function() {
-			var that = this;
-			T3.Content.Controller.ServerConnection.sendAllToServer(
-				this.get('publishableEntitySubjects'),
-				function(subject) {
+			var that = this,
+				transformFn = function(subject) {
 					var entity = vie.entities.get(subject);
 					return [entity.fromReference(subject)];
 				},
-				TYPO3_Neos_Service_ExtDirect_V1_Controller_WorkspaceController.discardNode,
-				function() {
-					require(
-						{context: 'neos'},
-						[
-							'Content/Application'
-						],
-						function(ContentModule) {
-							ContentModule.reloadPage();
-							ContentModule.one('pageLoaded', function() {
-								Ember.run.next(function() {
-									EventDispatcher.trigger('nodesInvalidated');
-									EventDispatcher.trigger('contentChanged');
-								});
-							});
-						}
-					);
-					var nodeTypeSchema = NodeTypeService.getCurrentNodeTypeSchema(),
-						title = $('#neos-page-metainformation').attr('data-neos-title')
-					Notification.ok('Discarded changes for ' + nodeTypeSchema.ui.label + ' "' + title + '".');
-				},
-				function(subject) {
-					var entity = vie.entities.get(subject);
+				numberOfUnsavedRecords = this.get('publishableEntitySubjects.length');
 
-					var nodePath = entity.id.substr(1, entity.id.lastIndexOf('@') - 1),
-						node = that.get('workspaceWidePublishableEntitySubjects').findBy('nodePath', nodePath);
-					if (node) {
-						that.get('workspaceWidePublishableEntitySubjects').removeObject(node);
+			this.get('publishableEntitySubjects').forEach(function(element) {
+				// Force copy of array
+				var args = transformFn(element).slice();
+				WorkspaceEndpoint.discardNode(args[0]).then(
+					function() {
+						that._removeNodeFromPublishableEntitySubjects(element);
+						numberOfUnsavedRecords--;
+						if (numberOfUnsavedRecords <= 0) {
+							require(
+								{context: 'neos'},
+								[
+									'Content/Application'
+								],
+								function(ContentModule) {
+									ContentModule.reloadPage();
+									ContentModule.one('pageLoaded', function() {
+										Ember.run.next(function() {
+											EventDispatcher.trigger('nodesInvalidated');
+											EventDispatcher.trigger('contentChanged');
+										});
+									});
+								}
+							);
+							var nodeType, title = $('#neos-page-metainformation').attr('data-neos-title');
+
+							// TODO: The fetching of the current node type should be handled using the NodeTypeService
+							// which at the moment causes issues with promises which still have to be optimized
+							ResourceCache.getItem(Configuration.get('NodeTypeSchemaUri') + '&superType=TYPO3.Neos:Document').then(
+								function(schema) {
+									nodeType = schema[$('#neos-page-metainformation').attr('data-neos-__nodetype')].ui.label;
+									Notification.ok('Discarded changes for ' + nodeType + ' "' + title + '"');
+								}
+							);
+						}
 					}
-				}
-			);
+				);
+			});
 		},
 
 		/**
 		 * Publishes everything inside the current workspace.
+		 *
+		 * @return {void}
 		 */
 		publishAll: function() {
 			var workspaceName = $('#neos-page-metainformation').attr('data-context-__workspacename'),
 				publishableEntities = this.get('publishableEntitySubjects'),
 				that = this;
-			TYPO3_Neos_Service_ExtDirect_V1_Controller_WorkspaceController.publishAll(workspaceName, function(result) {
-				if (typeof result !== 'undefined' && result !== null && result.success === true) {
+
+			WorkspaceEndpoint.publishAll(workspaceName).then(
+				function() {
 					$.each(publishableEntities, function(index, element) {
 						vie.entities.get(element).set('typo3:__workspacename', 'live');
 					});
 
 					that.getWorkspaceWideUnpublishedNodes();
-
 					Notification.ok('Published all changes.');
-				} else {
-					Notification.error('Unexpected error while publishing all changes: ' + JSON.stringify(result));
+				},
+				function(error) {
+					Notification.error('Unexpected error while publishing all changes: ' + JSON.stringify(error));
 				}
-			});
+			);
 		},
 
 		/**
 		 * Discards everything inside the current workspace.
+		 *
+		 * @return {void}
 		 */
 		discardAll: function() {
 			var workspaceName = $('#neos-page-metainformation').attr('data-context-__workspacename'),
 				that = this;
-			TYPO3_Neos_Service_ExtDirect_V1_Controller_WorkspaceController.discardAll(workspaceName, function(result) {
-				if (typeof result !== 'undefined' && result !== null && result.success === true) {
+			WorkspaceEndpoint.discardAll(workspaceName).then(
+				function() {
 					require(
 						{context: 'neos'},
 						[
@@ -210,24 +260,27 @@ define(
 					);
 					that.getWorkspaceWideUnpublishedNodes();
 					Notification.ok('Discarded all changes.');
-				} else {
-					Notification.error('Unexpected error while discarding all changes: ' + JSON.stringify(result));
+				},
+				function(error) {
+					Notification.error('Unexpected error while discarding all changes: ' + JSON.stringify(error));
 				}
-			});
+			);
 		},
 
 		/**
 		 * Get all unpublished nodes inside the current workspace.
+		 *
+		 * @return {void}
 		 */
 		getWorkspaceWideUnpublishedNodes: function() {
 			var workspaceName = $('#neos-page-metainformation').attr('data-context-__workspacename'),
 				that = this;
-			if (typeof TYPO3_Neos_Service_ExtDirect_V1_Controller_WorkspaceController !== 'undefined') {
-				TYPO3_Neos_Service_ExtDirect_V1_Controller_WorkspaceController.getWorkspaceWideUnpublishedNodes(workspaceName, function(result) {
-					that.set('workspaceWidePublishableEntitySubjects', result.data);
-				});
-			}
-		}
 
+			WorkspaceEndpoint.getWorkspaceWideUnpublishedNodes(workspaceName).then(
+				function(result) {
+					that.set('workspaceWidePublishableEntitySubjects', result.data);
+				}
+			);
+		}
 	}).create();
 });