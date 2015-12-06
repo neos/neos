neos/neos

commit 71989b5e686415788da4e00d8e03d4883cf24ae8

[TASK] Cache content dimensions backend request in client
Instead of fetching the content dimensions every time the backend is loaded,
the request is cached using the ``ResourceCache``. This eliminates a request
for something that rarely changes and is automatically updated when the
configuration changes.

TYPO3.Neos/Resources/Public/JavaScript/Content/Components/ContentDimensionController.js
@@ -71,7 +71,7 @@ function(
 		 */
 		_loadConfiguration: function() {
 			var that = this;
-			HttpRestClient.getResource('neos-service-contentdimensions').then(function(result) {
+			ResourceCache.getItem('neos-service-contentdimensions').then(function(result) {
 				var configuration = {};
 
 				$.each($('.contentdimensions', result.resource).children('li'), function(key, contentDimensionSnippet) {

commit afe789754a12a4344a19aab6d8616a3d923de6d3

[BUGFIX] Loading content dimensions for dimensions selector
Regression introduced with 71989b5e686415788da4e00d8e03d4883cf24ae8

Resolves: NEOS-1722

TYPO3.Neos/Resources/Public/JavaScript/Content/Components/ContentDimensionController.js
@@ -71,7 +71,7 @@ function(
 		 */
 		_loadConfiguration: function() {
 			var that = this;
-			ResourceCache.getItem('neos-service-contentdimensions').then(function(result) {
+			ResourceCache.getItem(Configuration.get('ContentDimensionsUri')).then(function(result) {
 				var configuration = {};
 
 				$.each($('.contentdimensions', result.resource).children('li'), function(key, contentDimensionSnippet) {

TYPO3.Neos/Resources/Public/JavaScript/Shared/Configuration.js
@@ -47,6 +47,10 @@ function(Ember, $) {
 					case 'EditPreviewDataUri':
 						this._data[key] = $('link[rel="neos-editpreviewdata"]').attr('href');
 						return this._data[key];
+
+					case 'ContentDimensionsUri':
+						this._data[key] = $('link[rel="neos-service-contentdimensions"]').attr('href');
+						return this._data[key];
 				}
 			}
 