neos/neos

commit 45310e85fbeb7ffde938060d0de049d64ee3145e

BUGFIX: Personal workspace is not always deleted when its user is deleted
This change fixes a bug in the central User Service which can lead to
orphaned personal workspaces of deleted users.

Prior to this change, a personal workspace was only deleted for accounts
which had the role `TYPO3.Neos:Editor`. However, that role isn't always
necessary for a user to have a personal workspace.

The fix will check for the more generic `TYPO3.Neos:AbstractEditor`
role instead.

TYPO3.Neos/Classes/TYPO3/Neos/Domain/Service/UserService.php
@@ -302,8 +302,7 @@ class UserService
      */
     public function deleteUser(User $user)
     {
-        $backendUserRole = $this->policyService->getRole('TYPO3.Neos:Editor');
-
+        $backendUserRole = $this->policyService->getRole('TYPO3.Neos:AbstractEditor');
         foreach ($user->getAccounts() as $account) {
             /** @var Account $account */
             if ($account->hasRole($backendUserRole)) {

commit d980d10d95c58db3544278dee84b28edc9acc305

BUGFIX: Remove role check when deleting workspaces for users
This was broken by 45310e85fbeb7ffde938060d0de049d64ee3145e.
Before, checks were done for the role TYPO3.Neos:Editor. However,
checking for AbstractEditor was pointless; workspaces are
now never deleted when a user is deleted. Since the deletePersonalWorkspace
method checks for existence anyway, there's no reason to do
this role check at all. If a user is deleted, his workspaces should be
deleted as well.

TYPO3.Neos/Classes/TYPO3/Neos/Domain/Service/UserService.php
@@ -302,12 +302,8 @@ class UserService
      */
     public function deleteUser(User $user)
     {
-        $backendUserRole = $this->policyService->getRole('TYPO3.Neos:AbstractEditor');
         foreach ($user->getAccounts() as $account) {
-            /** @var Account $account */
-            if ($account->hasRole($backendUserRole)) {
-                $this->deletePersonalWorkspace($account->getAccountIdentifier());
-            }
+            $this->deletePersonalWorkspace($account->getAccountIdentifier());
             $this->accountRepository->remove($account);
         }
 