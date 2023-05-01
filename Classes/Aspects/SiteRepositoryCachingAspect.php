neos/neos

commit 9ee7060ed362cc0db45e0fc715efad6290c73120

TASK: again fix linter :)

Classes/Aspects/SiteRepositoryCachingAspect.php
@@ -44,6 +44,10 @@ class SiteRepositoryCachingAspect
      * @var Domain|boolean
      */
     protected $domainForActiveRequest = false;
+
+    /**
+     * @var array<string,Site>
+     */
     private array $byNodeNameCache = [];
 
     /**
@@ -71,9 +75,11 @@ class SiteRepositoryCachingAspect
         if ($nodeName instanceof SiteNodeName) {
             $nodeName = $nodeName->value;
         }
-
         if (!array_key_exists($nodeName, $this->byNodeNameCache) || $this->environment->getContext()->isTesting()) {
             $site = $joinPoint->getAdviceChain()->proceed($joinPoint);
+            // make phpstan happy
+            assert(is_string($nodeName));
+            assert($site instanceof Site);
             $this->byNodeNameCache[$nodeName] = $site;
         }
         return $this->byNodeNameCache[$nodeName];

Classes/FrontendRouting/Projection/DocumentUriPathFinder.php
@@ -20,6 +20,10 @@ final class DocumentUriPathFinder implements ProjectionStateInterface
 {
     private ?ContentStreamId $liveContentStreamIdRuntimeCache = null;
     private bool $cacheEnabled = true;
+
+    /**
+     * @var array<string,DocumentNodeInfo>
+     */
     private array $getByIdAndDimensionSpacePointHashCache = [];
 
     public function __construct(
@@ -253,7 +257,7 @@ final class DocumentUriPathFinder implements ProjectionStateInterface
         return DocumentNodeInfo::fromDatabaseRow($row);
     }
 
-    public function disableCache()
+    public function disableCache(): void
     {
         $this->cacheEnabled = false;
         $this->getByIdAndDimensionSpacePointHashCache = [];

commit 506d0ebdf9a4f7be3e6507cdcc46d44816d7d201

TASK: fix SiteRepositoryCachingAspect, which I accidentally broke
broken in 9ee7060ed362cc0db45e0fc715efad6290c73120 trying to fix
the linter

Classes/Aspects/SiteRepositoryCachingAspect.php
@@ -46,7 +46,7 @@ class SiteRepositoryCachingAspect
     protected $domainForActiveRequest = false;
 
     /**
-     * @var array<string,Site>
+     * @var array<string,mixed>
      */
     private array $byNodeNameCache = [];
 
@@ -79,7 +79,6 @@ class SiteRepositoryCachingAspect
             $site = $joinPoint->getAdviceChain()->proceed($joinPoint);
             // make phpstan happy
             assert(is_string($nodeName));
-            assert($site instanceof Site);
             $this->byNodeNameCache[$nodeName] = $site;
         }
         return $this->byNodeNameCache[$nodeName];