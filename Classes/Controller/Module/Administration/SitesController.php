7a838e15d5e5d2166af7f7b7e17affc34d8875dc
Sebastian Helzle <sebastian@helzle.it>
2025-09-16 17:17:23 +0200

    TASK: Use sites nodetype when generating sites node

--- Classes/Controller/Module/Administration/SitesController.php
+++ Classes/Controller/Module/Administration/SitesController.php
@@ -397,7 +397,7 @@ class SitesController extends AbstractModuleController
         $this->persistenceManager->persistAll();
         $sitesNode = $rootNode->getNode(SiteService::SITES_ROOT_PATH);
         if ($sitesNode === null) {
-            $sitesNode = $rootNode->createNode(NodePaths::getNodeNameFromPath(SiteService::SITES_ROOT_PATH));
+            $sitesNode = $rootNode->createNode(NodePaths::getNodeNameFromPath(SiteService::SITES_ROOT_PATH), $this->nodeTypeManager->getNodeType('Neos.Neos:Sites'));
         }
         $siteNode = $sitesNode->createNode($nodeName, $siteNodeType);
         $siteNode->setProperty('title', $siteName);
