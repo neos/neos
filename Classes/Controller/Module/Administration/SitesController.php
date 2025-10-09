deb560ef7e87a5f0a20e585acc8734cadd276fea
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2025-10-09 15:51:14 +0200

    TASK: Add setting for `baseNodeType` of sites module

--- Classes/Controller/Module/Administration/SitesController.php
+++ Classes/Controller/Module/Administration/SitesController.php
@@ -234,7 +234,7 @@ class SitesController extends AbstractModuleController
     public function newSiteAction(?Site $site = null)
     {
         $sitePackages = $this->packageManager->getFilteredPackages('available', 'neos-site');
-        $documentNodeTypes = $this->nodeTypeManager->getSubNodeTypes('Neos.Neos:Site', false);
+        $documentNodeTypes = $this->nodeTypeManager->getSubNodeTypes($this->moduleConfiguration['settings']['baseNodeType'] ?? 'Neos.Neos:Site', false);
 
         $generatorServiceIsAvailable = $this->packageManager->isPackageAvailable('Neos.SiteKickstarter');
         $generatorServices = [];
