0e476b58d511897fafa9ad535c6a17e15c9f27b1
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2025-11-10 16:35:22 +0100

    TASK: Write warnings in YAML files at top of file and emit them also in CLI output

--- Migrations/Code/Version20251109115127.php
+++ Migrations/Code/Version20251109115127.php
@@ -3,6 +3,8 @@
 namespace Neos\Flow\Core\Migrations;
 
 use Neos\Utility\Arrays;
+use Neos\Utility\Files;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Adjust configuration to Neos 9
@@ -19,16 +21,71 @@ class Version20251109115127 extends AbstractMigration
 
     public function up(): void
     {
-        $this->processConfiguration('Settings', function (array &$configurationByReference) {
-            $configurationByReference = $this->rewriteDimensionConfiguration($configurationByReference);
-        }, true);
-
-        $this->processConfiguration('Routes', function (array &$configurationByReference) {
-            $configurationByReference = $this->rewriteFrontendRoutePartHandler($configurationByReference);
-        }, true);
+        $this->replaceInConfiguration('Settings', $this->rewriteDimensionConfiguration(...));
+        $this->replaceInConfiguration('Routes', $this->rewriteFrontendRoutePartHandler(...));
     }
 
-    public function rewriteDimensionConfiguration(array $parsed): array
+    /**
+     * Modified and improved {@see processConfiguration} which 1. does not work with realpaths and thus works during vfs testing and 2. allows to prepend warning comments to the yaml file.
+     */
+    final public function replaceInConfiguration(string $configurationType, \Closure $processor): void
+    {
+        if (is_dir($this->targetPackageData['path'] . '/Configuration') === false) {
+            return;
+        }
+
+        $yamlPathsAndFilenames = new \RegexIterator(
+            new \RecursiveIteratorIterator(
+                new \RecursiveDirectoryIterator($this->targetPackageData['path'] . '/Configuration'),
+            ),
+            '/\.yaml$/',
+            \RecursiveRegexIterator::GET_MATCH,
+        );
+
+        $configurationPathsAndFilenames = array_filter(array_keys(iterator_to_array($yamlPathsAndFilenames)),
+            function ($pathAndFileName) use ($configurationType) {
+                if (strpos(basename($pathAndFileName, '.yaml'), $configurationType) === 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        );
+
+        foreach ($configurationPathsAndFilenames as $pathAndFilename) {
+            $warnings = [];
+
+            $commentPrefix = sprintf('TODO %s ', $this->getIdentifier());
+            $onWarning = fn (string $comment) => $this->showWarning(sprintf('File %s: %s', Files::getRelativePath(
+                $this->targetPackageData['path'],
+                $pathAndFilename
+            ), $comment));
+
+            $addWarning = function (string $warning) use (&$warnings, $onWarning) {
+                $onWarning($warning);
+                $warnings[] = $warning;
+            };
+
+            $fileContent = file_get_contents($pathAndFilename);
+            $originalConfiguration = Yaml::parse($fileContent);
+            $configuration = $processor($originalConfiguration, $addWarning);
+            if ($configuration !== $originalConfiguration) {
+                // TODO do we need to preserve file header lines like in YamlSource::getHeaderFromFile()?
+                // but in that case we need to check if comments exist already
+                // if (str_contains($fileContent, '# ' . $commentPrefix)) {
+                //     ($onWarning)('No migration todo comments written as the migration was already run.');
+                // }
+                $contents = Yaml::dump($configuration, 99, 2);
+                $precedingComments = array_map(fn ($comment) => '# ' . $commentPrefix . $comment, $warnings);
+                if ($warnings) {
+                    $contents = implode("\n", $precedingComments) . "\n" . $contents;
+                }
+                file_put_contents($pathAndFilename, $contents);
+            }
+        }
+    }
+
+    public function rewriteDimensionConfiguration(array $parsed, \Closure $addWarning): array
     {
         if (isset($parsed['Neos']['ContentRepositoryRegistry']['contentRepositories']['default']['contentDimensions'])) {
             // we already have a Neos.ContentRepositoryRegistry.contentRepositories.default.contentDimensions key
@@ -62,7 +119,7 @@ class Version20251109115127 extends AbstractMigration
             if (isset($oldDimensionConfig['default'])) {
                 $defaultDimensionSpacePoint[$dimensionName] = $oldDimensionConfig['default'];
             } else {
-                $this->showWarning(sprintf('For content dimension "%s", did not find any default dimension value underneath "default". The defaultDimensionSpacePoint might be incomplete.', $dimensionName));
+                $addWarning(sprintf('For content dimension "%s", did not find any default dimension value underneath "default". The defaultDimensionSpacePoint might be incomplete.', $dimensionName));
             }
             foreach ($oldDimensionConfig['presets'] as $presetName => $presetConfig) {
                 // we need to use the last dimension value as the the new dimension value name; because that is
@@ -78,7 +135,7 @@ class Version20251109115127 extends AbstractMigration
                 }
 
                 if (!isset($presetConfig['values'])) {
-                    $this->showWarning(sprintf('For preset "%s", did not find any dimension values underneath "values"', $presetName));
+                    $addWarning(sprintf('For preset "%s", did not find any dimension values underneath "values"', $presetName));
                 } else {
                     $valuesExceptLast = $presetConfig['values'];
                     $valuesExceptLast = array_reverse($valuesExceptLast);
@@ -92,7 +149,7 @@ class Version20251109115127 extends AbstractMigration
                     if (isset($presetConfig['uriSegment'])) {
                         $uriPathSegmentsForDimension['dimensionValueMapping'][$lastValue] = $presetConfig['uriSegment'];
                     } else {
-                        $this->showWarning(sprintf('For preset "%s", did not find any uriSegment.', $presetName));
+                        $addWarning(sprintf('For preset "%s", did not find any uriSegment.', $presetName));
                     }
                 }
             }
